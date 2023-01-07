23a8cb936c4415c692d3f6a09ec596b1c012d58e
zzoaJW <i7can7i@gmail.com>
2023-01-07 19:06:18 +0900

    Functional Implementation : postDalay

--- app/src/main/java/com/z0o0a/lottietest/MainActivity.kt
+++ app/src/main/java/com/z0o0a/lottietest/MainActivity.kt
@@ -2,6 +2,9 @@ package com.z0o0a.lottietest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import com.z0o0a.lottietest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +17,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
 
+        binding.btnLogin.setOnClickListener {
+            showAnim()
+        }
+
+    }
+
+    private fun showAnim(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.loadingAnim.visibility = View.VISIBLE
+            hideAnim()
+        }, 3000) // 3초 뒤에 실행
+    }
+
+    private fun hideAnim(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.loadingAnim.visibility = View.GONE
+        }, 3000) // 3초 뒤에 실행
     }
 }
