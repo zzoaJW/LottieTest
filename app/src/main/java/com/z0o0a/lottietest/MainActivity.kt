841a66a3084ef6a60f37251a75197fa6ce69f46e
zzoaJW <i7can7i@gmail.com>
2023-01-07 17:27:27 +0900

    Functional Implementation : Lottie Animation in xml

--- app/src/main/java/com/z0o0a/lottietest/MainActivity.kt
+++ app/src/main/java/com/z0o0a/lottietest/MainActivity.kt
@@ -2,10 +2,17 @@ package com.z0o0a.lottietest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.z0o0a.lottietest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
