f73238564f39c0b60092b6d6a539933e812e16b8
zzoaJW <i7can7i@gmail.com>
2023-01-07 16:56:47 +0900

    Initial commit

---
+++ app/src/main/java/com/z0o0a/lottietest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.z0o0a.lottietest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
